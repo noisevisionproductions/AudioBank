a05a901a87f6ad8c252103ad00350c44d30bd798
Noise290 <noisemonster29@gmail.com>
2024-10-08 13:36:58 +0200

    created synchronization between storage and cosmos db using rest. now displaying duration of sounds correctly

--- composeApp/src/nativeMain/kotlin/org/noisevisionproductions/samplelibrary/database/CosmosService.native.kt
+++ composeApp/src/nativeMain/kotlin/org/noisevisionproductions/samplelibrary/database/CosmosService.native.kt
@@ -1,7 +1,9 @@
 package org.noisevisionproductions.samplelibrary.database
 
+import org.noisevisionproductions.samplelibrary.FileMetadata
+
 actual class CosmosService {
-    actual suspend fun getDataFromCosmos(): List<String> {
+    actual suspend fun getSynchronizedData(): List<FileMetadata> {
         TODO("Not yet implemented")
     }
 }
